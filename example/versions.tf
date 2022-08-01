79d510dc9de7e4e34c9e952d1b9eda672c407222
Tomáš Hejátko <tomas.hejatko@ackee.cz>
2022-08-01 16:30:43 +0200

    🔥 Remove key_algorithm parameter from tls_self_signed_cert resource
    Related: #73951

--- example/versions.tf
+++ example/versions.tf
@@ -1,21 +1,21 @@
 terraform {
-  required_version = "1.1.9"
+  required_version = "1.2.6"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.20.0"
+      version = "~> 4.30.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.20.0"
+      version = "~> 4.30.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3.3.0"
+      version = "~> 4.0.0"
     }
   }
 }
